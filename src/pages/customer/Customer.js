807148f5dc4f40f5bb39945e525c1ed8207fed6e
ayush572 <ayushdagaeinstein@gmail.com>
2023-04-29 09:11:03 +0530

    made the components to be used in dashboard

--- src/pages/customer/Customer.js
+++ src/pages/customer/Customer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Datatable from "../../components/datatable/Datatable";
 import Sidebar from "../../components/sidebar/Sidebar";
-import Navbar from "./../../components/navbar/Navbar";
+import Navbar from "./../../components/navbar/Navbardashboard";
 import "./customers.scss";
 
 const Customers = () => {
