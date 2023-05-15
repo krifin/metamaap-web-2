d17ebd56b50f15e73ca9594a3ad997d295daaca6
kunal528 <jainkunal976@gmail.com>
2023-05-15 18:02:32 +0530

    hosted the website in firebase hosting

--- src/pages/Preloader.js
+++ src/pages/Preloader.js
@@ -6,30 +6,31 @@ const Preloader = () => {
 
     // loader thw preloader for 5 seconds then start the video above the loading screen and when the video is done go to the home page
     useEffect(() => {
-        const timer = setTimeout(() => {
+        if (videoRef.current === null) return
+        const timer = setTimeout(async () => {
             document.querySelector('.preloader').classList.add('hide-preloader')
             videoRef.current.classList.add('show-video')
             videoRef.current.classList.remove('hide-video')
             videoRef.current.play()
             videoRef.current.addEventListener('ended', () => {
-                document.querySelector('.video-container').classList.add('hide-video')
-                document.querySelector('.video-container').classList.remove('show-video')
+                videoRef.current.classList.add('hide-video')
+                videoRef.current.classList.remove('show-video')
                 window.location.href = '/home'
             })
         }, 2000)
         return () => clearTimeout(timer)
-    }, [])
+    }, [videoRef])
     return (
         <div>
             <div className="preloader">
-                <div className="loader-circle">
-                    <p className="loader-content">LOADING</p>
-                    <div className="loader-line-mask">
-                        <div className="loader-line"></div>
+                <div className="preloader-circle">
+                    <p className="preloader-content">LOADING</p>
+                    <div className="preloader-line-mask">
+                        <div className="preloader-line"></div>
                     </div>
                 </div>
             </div>
-            <video ref={videoRef} className="video-container hide-video" autoPlay muted>
+            <video ref={videoRef} className="preloader-video-container hide-video" autoPlay muted>
                 <source src="/assets/videos/preloader_video.mp4" type="video/mp4" />
             </video>
         </div>
